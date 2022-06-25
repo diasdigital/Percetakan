27f4a61c734126fc4bf89f83af35d5c29ad20f82
diasdigital <78405123+diasdigital@users.noreply.github.com>
2022-06-25 16:52:47 +0700

    Memasang template frontend
    Memasukan asset kedalam publik
    Mengatur agar index menampilkan sesuai dengan template

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Frontend;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [Frontend::class, 'index'])->name('beranda');
+
